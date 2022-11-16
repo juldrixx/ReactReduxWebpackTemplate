1a60c807de4fa3f118a62f7fa3cb786ba1377f97
Julien Guitton <julien.guitton.ext@orange.com>
2022-11-16 11:46:24 +0100

    Update redux configuration

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,9 +1,10 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { applyMiddleware, compose } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
 const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, storeEnhancers(applyMiddleware(thunk)));
+const store = configureStore(rootReducer, storeEnhancers(applyMiddleware(thunk)));
 
 export default store;
